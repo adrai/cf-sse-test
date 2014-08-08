7dc8097bfae9dffb94575126ab9239142d04114f
Adriano Raiano <adriano@raiano.ch>
2014-08-08 10:35:02 +0200

    first commit

---
+++ client/app.js
@@ -0,0 +1,26 @@
+var EventSource = require('eventsource');
+
+var serverUrl = 'http://localhost:8080';
+
+var auth = false;
+
+var es = new EventSource(serverUrl + '/event', {
+  headers: {
+    'authorization': auth ? 'Basic ' + new Buffer('user'+ ':' + 'password').toString('base64') : undefined
+  }
+});
+es.onmessage = function(e) {
+  console.log(e.data);
+  // es.close();
+};
+es.onerror = function() {
+  console.log('ERROR!');
+};
+
+if (process.env.PORT) {
+  var http = require('http');
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Just for CF! ;-)\n');
+  }).listen(process.env.PORT);
+}
---
+++ server/app.js
@@ -0,0 +1,44 @@
+var restify = require('restify'),
+    passport = require('passport'),
+    BasicStrategy = require('passport-http').BasicStrategy,
+    sse = require('connect-sse');
+
+var restService = restify.createServer();
+
+var acceptable = [].concat(restService.acceptable);
+acceptable.push('text/event-stream');
+restService.use(restify.acceptParser(acceptable));
+restService.use(restify.queryParser());
+restService.use(restify.bodyParser());
+
+var auth = false;
+
+if (auth) {
+  passport.use(new BasicStrategy(
+    function(username, password, done) {
+      done(null, username === options.credentials.username &&
+                 password === options.credentials.password);
+    }
+  ));
+
+  restService.use(restify.authorizationParser());
+  restService.use(passport.initialize());
+  restService.use(passport.authenticate('basic', { session: false }));
+}
+
+restService.get('/event', sse(), function (req, res) {
+  var counter = 0;
+
+  req.on('close', function() {
+    console.log('closed connection');
+  });
+
+  res.json("this is an event");
+  setInterval(function() {
+    res.json({here: "is", another: "event", number: ++counter});
+  }, 1000);
+});
+
+restService.listen(process.env.PORT || 8080, function () {
+  console.log( '%s listening at %s', restService.name, restService.url);
+});
